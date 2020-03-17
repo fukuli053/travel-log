8958a02e5be7d76c802c24e4a1f4004adb7b2e66
Furkan Ergün <furkan.ergun@ogr.sakarya.edu.tr>
2020-03-17 23:42:24 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,24 @@
+#Full Stack Travel Log
+
+I did it with watching and following CodingGarden's video and I added some new feautres. 
+A full stack application to store / list places you have traveled.
+
+#TODO
+
+[x]Setup Server
+    [x]Install Dependencies
+    [x]Install / Setup Linter
+    [x]Setup Express App
+    [x]Setup Not Found and Error Middlewares
+[]Model DB
+    []What data will we store?
+[]Setup Mongoose Model(s)
+[]POST /logs
+    []Create a new log entry
+[]GET / logs
+    []List all log entries
+[]Setup Client
+[]Create Form to add a new entry
+[]Setup Map SDK on client
+[]List all log entries on map
+[]DEPLOY!
---
+++ server/src/index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const morgan = require('morgan'); //    is a loger
+const helmet = require('helmet'); //    is adding and deleting some headers for security.
+const cors = require('cors');
+
+const app = express();
+app.use(morgan('common'));
+app.use(helmet());
+app.use(cors({
+  origin: 'http://localhost:3000',
+}));
+
+app.get('/', (req, res) => {
+  res.json({
+    message: 'Hello World !',
+  });
+});
+
+app.use((req, res, next) => {
+  const error = new Error(`Not Found - ${req.originalUrl}`);
+  res.status(404);
+  next(error);
+});
+
+// eslint-disable-next-line no-unused-vars
+app.use((error, req, res, next) => {
+  const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+  res.status(statusCode);
+  res.json({
+    message: error.message,
+    stack: process.env.NODE_ENV === 'production' ? ':)' : error.stack,
+  });
+});
+
+const port = process.env.PORT || 1337;
+app.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}`);
+});
